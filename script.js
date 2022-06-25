27b56b2e4065023e7f8c5f582b3f56f1525b8c6c
Shreyash Gondane <50496886+marlin-spike@users.noreply.github.com>
2022-06-25 14:48:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 $(document).ready(function () {
-
   function second_passed() {
-    $('.clock').removeClass('is-off');
+    $(".clock").removeClass("is-off");
   }
-  setTimeout(second_passed, 2000)
+  setTimeout(second_passed, 2000);
 
-  $('.switcher').on('click', function(e) {
+  $(".switcher").on("click", function (e) {
     e.preventDefault();
-    $('.screen').toggleClass('glitch');
+    $(".screen").toggleClass("glitch");
   });
-
 
   var newDate = new Date();
   newDate.setDate(newDate.getDate());
 
-  setInterval( function() {
+  setInterval(function () {
+    var hours = new Date().getHours();
+    var seconds = new Date().getSeconds();
+    var minutes = new Date().getMinutes();
 
-    var hours    = new Date().getHours();
-    var seconds  = new Date().getSeconds();
-    var minutes  = new Date().getMinutes();
+    var realTime =
+      (hours < 10 ? "0" : "") +
+      hours +
+      " : " +
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      " : " +
+      (seconds < 10 ? "0" : "") +
+      seconds;
 
-    var realTime = ( hours < 10 ? '0' : '' ) + hours + ' : ' + ( minutes < 10 ? '0' : '' ) + minutes + ' : ' + ( seconds < 10 ? '0' : '' ) + seconds
-
-    $('.time').html(realTime);
-    $('.time').attr('data-time', realTime);
-
+    $(".time").html(realTime);
+    $(".time").attr("data-time", realTime);
   }, 1000);
-
 });
